d055b4c6890d72820a1f6b971a9075e9cb4d79e5
Dylan <leedylanrobert@gmail.com>
2022-06-04 19:58:26 -0400

    added doubly linked list and wip ear clip func

--- Assets/TrailCollider.cs
+++ Assets/TrailCollider.cs
@@ -3,8 +3,8 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utils;
  
-[RequireComponent(typeof(EdgeCollider2D))]
 //Add this script to a new gameobject at 0,0,0
 //make sure the gameobject position does not change.
 public class TrailCollider : MonoBehaviour
@@ -58,8 +58,15 @@ public class TrailCollider : MonoBehaviour
 
                         if (xDiff < errorMargin & yDiff < errorMargin)
                         {
+                            Debug.Log("CROSSED!");
                             crossed = true;
+                            Debug.Log("Vertices Count: " + positions.GetLength(0));
 
+                            // Filters out all points before the loop for calculating hitbox
+                            var onlyLoop = pointsInTrailRenderer.Skip(i+1).ToArray();
+                            Utils.DLL everyPoint =  ClipEars(onlyLoop);
+                            Debug.Log("Ear clips head prev: " + everyPoint.head.prev.prev.data);
+                            Debug.Log("Ear clips head next: " + everyPoint.head.prev.next.data);
                             Vector3[] freshList = new Vector3[]{};
                             _tr.Clear();
                         }
@@ -81,5 +88,70 @@ public class TrailCollider : MonoBehaviour
             Debug.Log("$$$$$$ Prev Point $$$$$$: " + prevPoint);
             Debug.Log("$$$$$$ Curr Point $$$$$$: " + currPoint);
         }
+    }
+
+    public static Utils.DLL ClipEars(Vector3[] polygonPoints)
+    {
+        double xMin = Math.Round(polygonPoints[0].x, 2);
+        int minNodeIndex = 0;
+        // Vector3 minVector = polygonPoints.First(point => point.x == xMin);
+
+        Utils.DLL.Node firstNode = new Utils.DLL.Node(0);
+        Utils.DLL allPoints = new Utils.DLL();
+        Utils.DLL.Node leftNode = firstNode;
+        allPoints.head = firstNode; 
+
+
+        Debug.Log("Polygonpoints length: " + polygonPoints.Length);
+
+        // Get index of the leftmost node which is certainly a convex node
+        for (int i=1; i < polygonPoints.Length; i++)
+        {    
+            Utils.DLL.Node nextNode = new Utils.DLL.Node(i);    
+            firstNode.next = nextNode;
+            nextNode.prev = firstNode;
+            firstNode = nextNode;
+            if (polygonPoints[i].x < xMin)
+            {
+                leftNode = firstNode;
+                minNodeIndex = i;
+            }
+        }
+        Debug.Log("Leftmost node index: " + minNodeIndex);
+
+        Utils.DLL convexPoints = new Utils.DLL();
+        Utils.DLL reflexPoints = new Utils.DLL();
+
+        // Utils.DLL.Node leftNode;
+        // Utils.DLL.Node middleNode;
+
+        for (int i=0; i < polygonPoints.Length; i++)
+        {    
+            int rightIndex = minNodeIndex + i;
+            int middleIndex = rightIndex - 1;
+            int leftIndex = rightIndex - 2;
+
+            Utils.DLL.Node rightNode = new Utils.DLL.Node(rightIndex);
+
+            if (i == 0)
+            {
+                convexPoints.head = rightNode;
+            }
+            else if (i == 1)
+            {}
+            else
+            {
+                float leftMiddleSide = Vector3.Distance(polygonPoints[leftIndex], polygonPoints[middleIndex]);
+                float rightMiddleSide = Vector3.Distance(polygonPoints[rightIndex], polygonPoints[middleIndex]);
+                float leftRightSide = Vector3.Distance(polygonPoints[leftIndex], polygonPoints[rightIndex]);
+
+                float angle = MathF.Acos(((leftMiddleSide*leftMiddleSide) + (rightMiddleSide*rightMiddleSide) - (leftRightSide*leftRightSide)) / (2 * leftMiddleSide * rightMiddleSide));
+            }
+        }
+
+        firstNode.next = allPoints.head;
+        allPoints.head.prev = firstNode;
+
+        return allPoints;
     }
 }
