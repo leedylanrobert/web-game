df94c0957a9e297dfe08c1ca675f24ca144ac14e
WongInitiative <keltonwong1@gmail.com>
2022-06-20 21:20:00 -0400

    fixed loop detection

--- Assets/TrailCollider.cs
+++ Assets/TrailCollider.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Utils;
 using Flat;
-using Converter;
  
 //Add this script to a new gameobject at 0,0,0
 //make sure the gameobject position does not change.
@@ -33,112 +32,131 @@ public class TrailCollider : MonoBehaviour
         Vector3[] pointsInTrailRenderer3d = new Vector3[_tr.positionCount]; 
         _tr.GetPositions(pointsInTrailRenderer3d);
 
-        Vector2[] pointsInTrailRenderer = pointsInTrailRenderer3d.toVector2Array(); 
+        Vector2[] pointsInTrailRenderer = ConvertArray(pointsInTrailRenderer3d); 
 
-        Vector2 lastPoint = pointsInTrailRenderer.Last();
-        double lastX = Math.Round(lastPoint.x, 2);
-        double lastY = Math.Round(lastPoint.y, 2);    
+        Debug.Log("Check 1");
+        Vector2 newPoint = pointsInTrailRenderer.Last();
+        double newX = newPoint.x;
+        double newY = newPoint.y;
 
-        if (lastX != currPoint.x | lastY != currPoint.y)
+        if (pointsInTrailRenderer.Length > 1)
         {
+            Vector2[] pointsWithoutNewest = pointsInTrailRenderer.SkipLast(1).ToArray();
+            Vector2 prevPoint = pointsWithoutNewest.Last();
 
-            if (lastX != prevPoint.x | lastY != prevPoint.y)
+            double prevX = prevPoint.x;
+            double prevY = prevPoint.y;
+
+
+            // Check if there was a hover, skip segment check if there was a hover
+            if (newX != prevX | newY != prevY)
             {
-                if (pointsInTrailRenderer.Length > 1)
+                // Check if each of the previous segments intersect with current segment
+                Vector2 tailPoint = pointsWithoutNewest[0];
+                Vector2 headPoint = pointsWithoutNewest[0];
+                for (int i=0; i < pointsWithoutNewest.Length; i++)
                 {
-                    Vector2[] pointsWithoutLast = pointsInTrailRenderer.SkipLast(1).ToArray();
-                    double[,] positions = new double[pointsWithoutLast.Length, 2];
-                    for(int i=0; i < pointsWithoutLast.Length; i++)
-                    {
-                        double positionX = Math.Round(pointsWithoutLast[i].x, 2);
-                        double positionY = Math.Round(pointsWithoutLast[i].y, 2);
-                        positions[i,0] = positionX;
-                        positions[i,1] = positionY;
-                    }
+                    tailPoint = headPoint;
+                    headPoint = pointsWithoutNewest[i];
 
-                    for(int i=0; i < positions.GetLength(0); i++)
+                    // Don't check segment if head and tail point are identical
+                    // Also don't check segment if head and prev point are identical
+                    if ((headPoint.x != tailPoint.x | headPoint.y != tailPoint.y) & (headPoint.x != prevPoint.x | headPoint.y != prevPoint.y))
                     {
-                        double errorMargin = 0.07;
-                        double xDiff = Math.Abs(lastX - positions[i,0]);
-                        double yDiff = Math.Abs(lastY - positions[i,1]);
-
-                        if (xDiff < errorMargin & yDiff < errorMargin)
+                        crossed = intersect(newPoint, prevPoint, headPoint, tailPoint);
+                        if (crossed)
                         {
-                            Debug.Log("CROSSED!");
-                            crossed = true;
-
- 
-                            // Filters out all points before the loop for calculating hitbox
-                            var onlyLoop = pointsInTrailRenderer.Skip(i+1).ToArray();
-
-                            bool meshTriangles = Triangulate(onlyLoop, out triangles, out string errorMessage);
-                            // Debug.Log("Triangle Vertex Count: " + triangles.Length);
-                            // Debug.Log("Vertex Count: " + onlyLoop.Length);
-
-                            // Restart trail on loop
-                            _tr.Clear();
+                            Debug.Log("New Point:" + newPoint.x + ", " + newPoint.y);
+                            Debug.Log("Prev Point: " + prevPoint.x + ", " + prevPoint.y);
+                            Debug.Log("Head Point: " + headPoint.x + ", " + headPoint.y);
+                            Debug.Log("Tail Point: " + tailPoint.x + ", " + tailPoint.y);
                         }
                     }
                 }
             }
-            prevPoint = currPoint;
-            currPoint = pointsInTrailRenderer.Last();
+        }
+
+        
+        if (crossed)
+        {
+            Debug.Log("CROSS!!!!!!!!!!!!!!!!");
         }
     }
 
-    public static Vector2[] FilterPoints(Vector2[] vertices) 
+     Vector2[] ConvertArray(Vector3[] v3){
+     Vector2 [] v2 = new Vector2[v3.Length];
+     for(int i = 0; i <  v3.Length; i++){
+         Vector3 tempV3 = v3[i];
+         v2[i] = new Vector2(tempV3.x, tempV3.y);
+     }
+     return v2;
+ }
+
+    public bool ccw(Vector2 A, Vector2 B, Vector2 C)
     {
-        List<Vector2> finalVertices = new List<Vector2>();
-        for (int i=0; i < vertices.Length; i++)
-        {
-            leftIndex = i - 1 < 0 ? vertices.Length - 1 : i - 1;
-            rightIndex = i + 1 > vertices.Length - 1 ? 0 : i + 1;
-
-            currentVertex = vertices[i];
-            nextVertex = vertices[rightIndex];
-
-            // Check if current point is identical to next
-            if (currentVertex.x != nextVertex.x | currentVertex.y != nextVertex.y)
-            {
-                // Point is not identical to next
-                // Check if point is on vector between previous and next vertex
-                dxc = currPoint.x - point1.x;
-                dyc = currPoint.y - point1.y;
-
-                dxl = point2.x - point1.x;
-                dyl = point2.y - point1.y;
-
-                cross = dxc * dyl - dyc * dxl;
-
-                if (cross == 0)
-                {
-                    if (abs(dxl) >= abs(dyl))
-                    {
-                        return dxl > 0 ? 
-                        point1.x <= currPoint.x && currPoint.x <= point2.x :
-                        point2.x <= currPoint.x && currPoint.x <= point1.x;
-                    }
-                    else
-                    {
-                        return dyl > 0 ? 
-                        point1.y <= currPoint.y && currPoint.y <= point2.y :
-                        point2.y <= currPoint.y && currPoint.y <= point1.y;
-                    }
-                }
-                // Point is not between previous and next so we add to final vertices
-                finalVertices.Add(currentVertex);
-            }
-
-            // float leftMiddleSide = Vector3.Distance(vertices[leftIndex], vertices[i]);
-            // float rightMiddleSide = Vector3.Distance(vertices[rightIndex], vertices[i]);
-            // float leftRightSide = Vector3.Distance(vertices[leftIndex], vertices[rightIndex]);
-
-            // float pi = (float)Math.PI;
-            // float angle = (180 / pi) * MathF.Acos(((leftMiddleSide*leftMiddleSide) + (rightMiddleSide*rightMiddleSide) - (leftRightSide*leftRightSide)) / (2 * leftMiddleSide * rightMiddleSide));
-
-        }    
-        return finalVertices.ToArray();
+        return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x);
     }
+
+    public bool intersect(Vector2 A, Vector2 B, Vector2 C, Vector2 D)
+    {
+        return ccw(A,C,D) != ccw(B,C,D) & ccw(A,B,C) != ccw(A,B,D);
+    }
+
+    // public static Vector2[] FilterPoints(Vector2[] vertices) 
+    // {
+    //     List<Vector2> finalVertices = new List<Vector2>();
+    //     for (int i=0; i < vertices.Length; i++)
+    //     {
+    //         int leftIndex = i - 1 < 0 ? vertices.Length - 1 : i - 1;
+    //         int rightIndex = i + 1 > vertices.Length - 1 ? 0 : i + 1;
+
+    //         Vector2 prevVertex = vertices[leftIndex];
+    //         Vector2 currentVertex = vertices[i];
+    //         Vector2 nextVertex = vertices[rightIndex];
+
+    //         // Check if current point is identical to next
+    //         if (currentVertex.x != nextVertex.x | currentVertex.y != nextVertex.y)
+    //         {
+    //             // Point is not identical to next
+    //             // Check if point is on vector between previous and next vertex
+    //             float dxc = currentVertex.x - prevVertex.x;
+    //             float dyc = currentVertex.y - prevVertex.y;
+
+    //             float dxl = nextVertex.x - prevVertex.x;
+    //             float dyl = nextVertex.y - prevVertex.y;
+
+    //             float cross = dxc * dyl - dyc * dxl;
+
+    //             if (cross == 0)
+    //             //point is on the line, check if it's between two vertices
+    //             {
+    //                 if (Math.Abs(dxl) >= Math.Abs(dyl))
+    //                 {
+    //                     return dxl > 0 ? 
+    //                     prevVertex.x <= currentVertex.x && currentVertex.x <= nextVertex.x :
+    //                     nextVertex.x <= currentVertex.x && currentVertex.x <= prevVertex.x;
+    //                 }
+    //                 else
+    //                 {
+    //                     return dyl > 0 ? 
+    //                     prevVertex.y <= currentVertex.y && currentVertex.y <= nextVertex.y :
+    //                     nextVertex.y <= currentVertex.y && currentVertex.y <= prevVertex.y;
+    //                 }
+    //             }
+    //             // Point is not between previous and next so we add to final vertices
+    //             finalVertices.Add(currentVertex);
+    //         }
+
+    //         // float leftMiddleSide = Vector3.Distance(vertices[leftIndex], vertices[i]);
+    //         // float rightMiddleSide = Vector3.Distance(vertices[rightIndex], vertices[i]);
+    //         // float leftRightSide = Vector3.Distance(vertices[leftIndex], vertices[rightIndex]);
+
+    //         // float pi = (float)Math.PI;
+    //         // float angle = (180 / pi) * MathF.Acos(((leftMiddleSide*leftMiddleSide) + (rightMiddleSide*rightMiddleSide) - (leftRightSide*leftRightSide)) / (2 * leftMiddleSide * rightMiddleSide));
+
+    //     }    
+    //     return finalVertices.ToArray();
+    // }
 
     public static bool Triangulate(Vector2[] vertices, out int[] triangles, out string errorMessage)
     {
