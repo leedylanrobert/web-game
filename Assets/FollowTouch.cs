0639542c776476135863c311648669a60defe48f
Dylan <leedylanrobert@gmail.com>
2022-07-08 00:30:28 -0400

    spider moving sprites with direction

--- Assets/FollowTouch.cs
+++ Assets/FollowTouch.cs
@@ -24,14 +24,62 @@ public class FollowTouch : MonoBehaviour
     float volume = 1;
     public bool collided = false;
 
+    private Animator anim;
+    private int direction;
+    // int direction guide: 0: down, 1: up, 2: left, 3: right
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
+        direction = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 currentPosition = transform.position;
+        if (targetPosition != null & targetPosition != currentPosition) 
+        {
+            float xdiff = currentPosition.x - targetPosition.x;
+            float ydiff = currentPosition.y - targetPosition.y;
+            
+            if (Mathf.Abs(xdiff) >= Mathf.Abs(ydiff)) 
+            {
+                if (xdiff >= 1)
+                {
+                    Debug.Log("Left");
+                    direction = 2;
+                }
+                else
+                {
+                    Debug.Log("Right");
+                    direction = 3;
+                }
+            }
+            else
+            {
+                if (ydiff >= 1)
+                {
+                    Debug.Log("Down");
+                    direction = 0;
+                }
+                else
+                {
+                    Debug.Log("Up");
+                    direction = 1;
+                }
+            }
+            Debug.Log("Moving");
+            anim.SetBool("moving", true);
+            anim.SetInteger("direction", direction);
+        }
+        else
+        {
+            Debug.Log("Idle");
+            anim.SetBool("moving", false);
+        }
+
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
