ee9bedc7a547b0d58174658f0c5b920c487921dc
Sophia Lee <slee600@binghamton.edu>
2022-10-11 23:42:21 -0400

    pause"

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -5,6 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class GameMaster : MonoBehaviour
 {
+    private bool isPaused = false;
+    private AudioSource backgroundMusic;
+    private AudioSource audioSource;
+    public AudioClip pause;
+
+    void Start()
+    {
+        backgroundMusic = GameObject.FindGameObjectWithTag("Background Music").GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if (sceneName == "MainMenu" & Input.GetKeyDown(KeyCode.Return))
+        {
+            GoToGameScene();
+        }
+        if (sceneName == "Game" & (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape)))
+        {
+            Debug.Log("Is Paused? " + isPaused);
+            if (!isPaused)
+            {
+                backgroundMusic.Pause();
+                audioSource.PlayOneShot(pause, 0.75f);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                backgroundMusic.UnPause();
+            }
+            isPaused = !isPaused;
+        }
+    }
+
     public void GoToGameScene() {
         SceneManager.LoadScene("Game");
     }
@@ -18,7 +54,6 @@ public class GameMaster : MonoBehaviour
     }
 
     public void QuitGame() {
-        Debug.Log("quit");
         Application.Quit();
     }
 
