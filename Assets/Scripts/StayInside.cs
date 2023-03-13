d3b6840b519972e4809309bad08f4c41c06a76a2
Sophia Lee <slee600@binghamton.edu>
2023-03-13 19:59:37 -0400

    messing around with resolution

--- Assets/Scripts/StayInside.cs
+++ Assets/Scripts/StayInside.cs
@@ -10,16 +10,22 @@ public class StayInside : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Position: " + transform.position);
+
         Camera camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         Vector3 topRightWorld = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
 
         xMax = topRightWorld.x;
         yMax = topRightWorld.y;
+
+        Debug.Log("Xmax: " + xMax);
+        Debug.Log("Ymax: " + yMax);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Position: " + transform.position);
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -xMax, xMax), Mathf.Clamp(transform.position.y, -yMax, yMax), transform.position.z);
     }
 }
